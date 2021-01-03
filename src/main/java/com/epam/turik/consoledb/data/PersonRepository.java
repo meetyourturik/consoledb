9d256e385a8c6db4f2b02bdd89a90496cfc0c8e4
ilya turov <meetyourturik@gmail.com>
2021-01-03 13:08:55 +0300

    working test

--- src/main/java/com/epam/turik/consoledb/data/PersonRepository.java
+++ src/main/java/com/epam/turik/consoledb/data/PersonRepository.java
@@ -2,7 +2,9 @@ package com.epam.turik.consoledb.data;
 
 import com.epam.turik.consoledb.data.objects.Person;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 
 }
--- src/test/java/com/epam/turik/consoledb/DummyTest.java
+++ src/test/java/com/epam/turik/consoledb/DummyTest.java
@@ -1,29 +1,53 @@
 package com.epam.turik.consoledb;
 
-import com.epam.turik.consoledb.config.TestConfig;
+import com.epam.turik.consoledb.application.PeopleWorker;
+import com.epam.turik.consoledb.application.Settings;
 import com.epam.turik.consoledb.data.PersonRepository;
 import com.epam.turik.consoledb.data.objects.Person;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.TemporaryFolder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.shaded.org.apache.commons.io.FileUtils;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+@Slf4j
 @Testcontainers
-@SpringBootTest(classes = TestConfig.class)
+@SpringBootTest
 class DummyTest {
+	@MockBean
+	Application application;
+
+	@ClassRule
+	String[] names = {"Petya", "Vasya", "Ilya", "Nastya", "Katya", "Pasha", "Lenya", "Jenya"};
+
+	@ClassRule
+	Random r = new Random();
+
 	@Autowired
-	ApplicationArguments args;
+	PeopleWorker worker;
 
 	@Autowired
 	PersonRepository personRepository;
+
+	@MockBean
+	Settings settings;
 
 	@Container
 	public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:10.14")
@@ -38,10 +62,60 @@ class DummyTest {
 		registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
 	}
 
+	@SneakyThrows
 	@Test
 	void test() {
-		for (Person p : personRepository.findAll()) {
-			System.out.println(p);
+		TemporaryFolder temporaryFolder = new TemporaryFolder();
+		temporaryFolder.create();
+		File peopleFile = temporaryFolder.newFile("people.json");
+		when(settings.getFilePath()).thenReturn(peopleFile.getAbsolutePath());
+		Person[] people = generatePeople();
+		writeToFile(peopleFile, people);
+		worker.doWork();
+		List<Person> peopleList = new ArrayList<>();
+		personRepository.findAll().forEach(peopleList::add);
+		Person[] newPeople = peopleList.toArray(new Person[0]);
+		assertEquals(newPeople.length, people.length);
+		for (int i = 0; i < newPeople.length; i++) {
+			assert(newPeople[i].getHascat() == people[i].getHascat() && newPeople[i].getName().equals(people[i].getName()) && newPeople[i].getAge().equals(people[i].getAge()));
 		}
 	}
+
+	@SneakyThrows
+	void writeToFile(File file, Person[] people) {
+		FileUtils.writeStringToFile(file, generatePeopleFile(people));// generatePeopleFile(generatePeople()));
+	}
+
+	private String generatePeopleFile(Person[] people) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("[");
+		for (Person p : people) {
+			sb.append("{\"name\":\"")
+					.append(p.getName())
+					.append("\",\"age\":")
+					.append(p.getAge())
+					.append(",\"hascat\":")
+					.append(p.getHascat())
+					.append("},");
+		}
+		return sb.substring(0, sb.length() - 1) + "]";
+	}
+
+	private Person[] generatePeople() {
+		int n = r.nextInt(15) + 1;
+		Person[] result = new Person[n];
+		for (int i = 0; i <n; i++) {
+			result[i] = randomPerson();
+		}
+		return result;
+	}
+
+	private Person randomPerson() {
+		Person person = new Person();
+		person.setName(names[r.nextInt(names.length)]);
+		person.setAge(r.nextInt(17) + 17);
+		person.setHascat(r.nextBoolean());
+		return person;
+	}
+
 }
